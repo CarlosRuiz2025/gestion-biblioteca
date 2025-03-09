18561defb97043b81d7f0b67a8e11f93b2d4f29f
Carlos Ruiz <andresruizmiranda3000@gmail.com>
2025-03-09 17:57:11 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+
+Route::get('/css/{file}', function ($file) {
+    $path = resource_path('css/' . $file);
+    if (file_exists($path)) {
+        return Response::file($path, ['Content-Type' => 'text/css']);
+    }
+    abort(404, 'Archivo no encontrado');
+});
 
 Route::get('/', function () {
     return view('welcome');
